2a20da6f743f6146fa14e58091eb762f881fe6fc
breadwithmeth <sergeq2222@gmail.com>
2023-04-20 01:28:19 +0600

    +

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,6 @@ class Main extends StatefulWidget {
 class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+    return  Placeholder();
+  } 
 }
